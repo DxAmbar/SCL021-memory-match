164ec6b4fc41961475aab55076b482aa93de93ea
CataGarciaRojas <cattapgarcia@gmail.com>
2022-08-04 17:09:04 -0400

    testing

--- src/components/App.js
+++ src/components/App.js
@@ -14,15 +14,69 @@
 //   .catch(console.error);
 //
 import pokemon from '../data/pokemon/pokemon.js';
-console.log(pokemon)
+console.log(pokemon.items);
+
+const pokemonCards = pokemon.items
 
 const App = () => {
-  const el = document.createElement('div');
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
+const root = document.querySelector('.root')
+let cardsChosen = []
+let cardsChosenId = []
 
-  return el;
+// primera función
+function createBoard(){
+    for(let i = 0; i<pokemon.items.length; i++){
+      let card = document.createElement('img')
+      card.setAttribute('src', 'img/levels/lvl1/pokelvl1.png')
+      card.setAttribute('data-id', i)
+      card.addEventListener('click', flipCard)
+      root.appendChild(card)
+    }
+}
+// second function
+function flipCard(){
+  let cardId = this.getAttribute('data-id')
+  cardsChosen.push(pokemonCards[cardId].name)
+  cardsChosenId.push(cardId)
+  this.setAttribute('src', pokemonCards[cardId].img)
+}
+
+createBoard()
+
 };
 
-export default App;
+export default App
+
+/*const pokemonCard = pokemon.items; 
+const doubledCards = pokemonCard.concat(pokemonCard);
+
+
+  const el = document.createElement('div');
+  el.className = 'App';
+  el.innerHTML = `<table> <tr> 
+  <td>
+   <p>${pokemon.items[0].id}</p> 
+  <img src="${pokemon.items[0].image}">
+  </td>
+</tr>
+</table>`;
+
+  return el;
+
+
+<section class="section1">
+            <table>
+                <tr>
+                "
+                    <td><button id="0" onclick="uncover(0)"></button></td>
+                    <td><button id="1" onclick="uncover(1)"></button></td>
+                    <td><button id="2" onclick="uncover(2)"></button></td>
+                </tr>
+                <tr>
+                    <td><button id="3" onclick="uncover(3)"></button></td>
+                    <td><button id="4" onclick="uncover(4)"></button></td>
+                    <td><button id="5" onclick="uncover(5)"></button></td>
+                </tr>
+            </table>
+        </section>*/
--- src/data/pokemon/pokemon.js
+++ src/data/pokemon/pokemon.js
@@ -1,12 +1,12 @@
 export default {
   name: 'Pokemon',
   items: [
-    { id: 'eevee', image: './img/lvl1/eevee.png', bgColor: '#339933' },
-    { id: 'pika', image: './img/lvl1/pika.png', bgColor: '#339933' },
-    { id: 'meowth', image: './img/lvl1/meowth.png', bgColor: '#339933' },
-    { id: 'ditto', image: './img/lvl2/ditto.png', bgColor: '#339933' },
-    { id: 'marill', image: './img/lvl2/marill.png', bgColor: '#F05032' },
-    { id: 'chiko', image: './img/lvl3/chiko.png', bgColor: '#F05032' },
-    { id: 'snorlax', image: './img/lvl3/snorlax.png', bgColor: '#1572B6' },
+    { id: 'eevee', image: './img/levels/lvl1/eevee.png', bgColor: '#339933' },
+    { id: 'pika', image: './img/levels/lvl1/pika.png', bgColor: '#339933' },
+    { id: 'meowth', image: './img/levels/lvl1/meowth.png', bgColor: '#339933' },
+    { id: 'ditto', image: './img/levels/lvl2/ditto.png', bgColor: '#339933' },
+    { id: 'marill', image: './img/levels/lvl2/marill.png', bgColor: '#F05032' },
+    { id: 'chiko', image: './img/levels/lvl3/chiko.png', bgColor: '#F05032' },
+    { id: 'snorlax', image: './img/levels/lvl3/snorlax.png', bgColor: '#1572B6' },
   ],
 };
